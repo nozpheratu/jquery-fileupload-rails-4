e5ad208ffeca4036bb5bbcc208abb52aecae661a
Cyle <cylehunter33@gmail.com>
2014-05-20 10:47:59 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JqueryFileuploadRails4::Application.config.secret_key_base = '046c696a3f5df4daaaae6f59fe7415362bd0d640599cd4fad0c0f257934dc11073125a9f04f79e6817bb9a48650f81feae80db6637c79ba6c8f66cb81d15e49c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JqueryFileuploadRails4::Application.config.session_store :cookie_store, key: '_jquery-fileupload-rails-4_session'
